dff361f990407d141ffa4225db9274bd2d87e268
BraveraTech <braverachacha@gmail.com>
2026-02-14 21:49:41 +0300

    fix: fixed contact file

--- api/app/auth/contact.py
+++ api/app/auth/contact.py
@@ -8,60 +8,47 @@ contact_ns = Namespace('contact', description='Contact related endpoint', path='
 # document models
 
 contact_model = contact_ns.model(
-    'Message',{
-        'first_name': fields.String(required=True),
-        'last_name': fields.String(required=True),
-        'email': fields.String(required=True),
-        'subject': fields.String(required=True),
-        'message': fields.String(required=True)
-    }
+  'Message',{
+    'first_name': fields.String(required=True),
+    'last_name': fields.String(required=True),
+    'email': fields.String(required=True),
+    'subject': fields.String(required=True),
+    'message': fields.String(required=True)
+  }
 )
 
 @contact_ns.route('/contact')
-class Contact(Resource):
-    @contact_ns.expect(contact_model)
-    def post(self):
-
-        data = request.get_json()
-       
-from flask import current_app
-
-protected_ns = Namespace('protected', description='Protected jwt endpoint', path='/auth')
-
-@protected_ns.route('/protected')
 class Protect(Resource):
-    def post(self):
-        return{
+  def post(self):
+    data = request.get_json()
+    first_name=data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    subject = data.get('subject')
+    message = data.get('message')
 
-        } 
-        first_name=data.get('first_name')
-        last_name = data.get('last_name')
-        email = data.get('email')
-        subject = data.get('subject')
-        message = data.get('message')
+    if not first_name or not last_name or not email or not subject or not message:
+      return{
+          'msg': 'All fields are required.'
+        }, 400
 
-        if not first_name or not last_name or not email or not subject or not message:
-            return{
-                'msg': 'All fields are required.'
-            }, 400
+    if not '@' in email or not '.' in email:
+      return{
+          'msg': 'Enter a valid email.'
+        }, 400
 
-        if not '@' in email or not '.' in email:
-            return{
-                'msg': 'Enter a valid email.'
-            }, 400
+    new_message = Message(
+      first_name=first_name,
+      last_name=last_name,
+      email=email,
+      subject=subject,
+      message=message
+    )
 
-        new_message = Message(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            subject=subject,
-            message=message
-        )
+    db.session.add(new_message)
+    db.session.commit()
+    db.session.close()
 
-        db.session.add(new_message)
-        db.session.commit()
-        db.session.close()
-
-        return{
-            'msg': 'Your message have been submitted successfully.'
-        }, 201
+    return{
+      'msg': 'Your message have been submitted successfully.'
+    }, 201
