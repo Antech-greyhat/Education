51c3e660b82c4e6bd2a9e4ffab98596662821f9d
braverachacha <braverachacha@gmail.com>
2026-02-21 21:13:05 +0300

    feat: Added real password reset link via email

---
+++ frontend/scripts/auth/resetPassword.js
@@ -0,0 +1,8 @@
+import { API_URL } from './config.js';
+import { dataSubmit } from './dataSubmit.js';
+
+const emailElement = document.querySelector('.js-reset-email');
+const resetButton = document.querySelector('.js-reset-button');
+
+console.log(emailElement); 
+console.log(resetButton); 
