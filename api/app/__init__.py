1c9e57f0aa88313c0f0c1b94fc1cf78e8e78740c
braverachacha <braverachacha@gmail.com>
2026-03-07 19:00:36 +0300

    feat: add radis for rate limiting and mail deafault sender

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -68,6 +68,7 @@ def create_app():
     app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
--- api/app/extensions.py
+++ api/app/extensions.py
@@ -4,9 +4,22 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+STORAGE_URI=os.environ.get("REDIS_URL")
+
+if not STORAGE_URI:
+    raise RuntimeError("REDIS_URL environment variable is not set!")
 
 db = SQLAlchemy()
 mail = Mail()
 jwt = JWTManager()
 migrate = Migrate()
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+  key_func=get_remote_address,
+  storage_uri=STORAGE_URI
+  )
