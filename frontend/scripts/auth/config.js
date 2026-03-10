cedf6fe35ae6f01cdbf04e7e78b2290eeecfc04a
braverachacha <braverachacha@gmail.com>
2026-03-10 13:08:57 +0300

    fix: used render api for authentucation

--- frontend/scripts/auth/config.js
+++ frontend/scripts/auth/config.js
@@ -1,9 +1,10 @@
-export let API_URL = 'https://education-ejxa.vercel.app';
+export let API_URL = 'https://antechapi.onrender.com';
 
-// console.log(API_URL);
 
 // FOR TESTING & DEVELOPMENT
 
-API_URL = 'https://antechlearn.ngrok.app';
+// API_URL = 'https://antechlearn.ngrok.app';
 
-API_URL = 'http://127.0.0.1:2000';
+// API_URL = 'http://127.0.0.1:2000';
+
+console.log(API_URL);
