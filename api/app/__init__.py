c3b64969660a257e348dc669c957663d4cbc04e9
braverachacha <braverachacha@gmail.com>
2026-03-22 15:52:38 +0300

    fix: apply refactor frontend and api to main

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -51,7 +51,7 @@ def create_app():
     # CORS
     CORS(
         app,
-        origins=[frontend_url, 'http://127.0.0.1:35729', 'http://localhost:8158', 'https://antech.ngrok.pro'],
+        origins=[frontend_url, 'http://localhost:8158', 'http://localhost:3000'],
         supports_credentials=True
     )
 
